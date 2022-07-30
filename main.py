372bd0f060255928ccb3f93a07d266524cf60b3e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-30 12:15:49 +0430

    Defining the root api

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+
+# Defining the api object
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return Response(status_code=200, content="The server is running.")
