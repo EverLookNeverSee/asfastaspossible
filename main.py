734305421bb427230ff38fd80dd9625b2aed21fa
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-30 12:26:36 +0430

    Adding inventory items

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ def root():
 @app.get("/about")
 def about():
     return {"message": "About page"}
+
+
+inventory = {
+    1: {"name": "banana", "price": "$1.00"},
+    2: {"name": "apple", "price": "$2.00"},
+    3: {"name": "orange", "price": "$3.00"},
+    4: {"name": "pear", "price": "$4.00"},
+    5: {"name": "grape", "price": "$5.00"},
+}
